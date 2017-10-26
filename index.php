66cd12102b16daf29811fa14123eb621c4870027
Hesitate123 <1045247746@qq.com>
2017-10-26 16:59:49 +0800

    优化了首页代码

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
 		<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport">
 		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 		<meta http-equiv="pragma" content="no-cache">
-	    <meta http-equiv="cache-control" content="no-cache">
-	    <meta http-equiv="expires" content="0">    
-	    <meta http-equiv="keywords" content="keyword1,keyword2,keyword3">
-	    <meta http-equiv="description" content="This is my page">
+		<meta http-equiv="cache-control" content="no-cache">
+		<meta http-equiv="expires" content="0">    
+		<meta http-equiv="keywords" content="keyword1,keyword2,keyword3">
+		<meta http-equiv="description" content="This is my page">
 		<title></title>
 		<link rel="stylesheet" href="css/font-awesome.css" />
         <link rel="stylesheet" type="text/css" href="css/angular-style.css"/>
